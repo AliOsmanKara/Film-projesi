e9f9e420f5042215f30c6e6809f187d41b444d04
Ali Osman Kara <Aokara3302@gmail.com>
2023-04-13 13:19:25 +0300

    adding movies from the storage to the interface

--- ui.js
+++ ui.js
@@ -3,18 +3,7 @@ function UI() {
 }
 UI.prototype.addFilmToUI = function (newFilm) {
 
-    /* <tr>
-    <td><img src="" class="img-fluid img-thumbnail"></td>
-    <td></td>
-    <td></td>
-    <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-  </tr> -->
-  <!-- <tr>
-    <td><img src="" class="img-fluid img-thumbnail"></td>
-    <td></td>
-    <td></td>
-    <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
-  </tr> */
+
     const filmList = document.getElementById("films");
 
     filmList.innerHTML += `
@@ -47,4 +36,20 @@ UI.prototype.displayMessages = function (message, type) {
     setTimeout(function () {
         div.remove();
     }, 1000);
+}
+
+UI.prototype.loadAllFilms = function (films) {
+    const filmList = document.getElementById("films");
+
+    films.forEach(function (film) {
+        filmList.innerHTML += `
+        <tr>
+            <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+            <td>${film.title}</td>
+            <td>${film.director}</td>
+             <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+        </tr>
+
+`
+    });
 }
